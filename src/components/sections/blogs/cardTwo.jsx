02ac2e20900080a6113dfc4157d34d7e5d243ab3
Leejoongjae123 <ljj3347@naver.com>
2024-08-08 23:11:44 +0900

    240808

--- src/components/sections/blogs/cardTwo.jsx
+++ src/components/sections/blogs/cardTwo.jsx
@@ -9,9 +9,9 @@ const CardTwo = ({ id, title, date, thumb, author, category }) => {
         <div className='group'>
             <ZoomIn id={id}>
                 <div className='relative overflow-hidden  rounded-2.5xl after:absolute after:w-full after:h-full after:top-0 after:left-0 after:bg-[linear-gradient(0deg,_rgba(0,0,0,0.40)_0%,_rgba(0,0,0,0.40)100%)] after:rounded-2.5xl'>
-                    <div className='absolute z-10 top-5 left-5'>
+                    {/* <div className='absolute z-10 top-5 left-5'>
                         <Link href={"/categories"} className='px-2.5 py-[5px] inline-block font-semibold rounded-lg text-base bg-background text-muted-foreground'>{category[0]}</Link>
-                    </div>
+                    </div> */}
                     <div className='relative h-[40vh]'>
                         <Image src={thumb} fill sizes='(max-width:640px) 100vw, (max-width:1024px) 50vw, 33vw' style={{ width: "100%" }} alt='thumb' className='group-hover:scale-110 transition-all duration-700' />
                     </div>
