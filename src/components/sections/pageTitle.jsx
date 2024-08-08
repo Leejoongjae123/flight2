7e181561af7613929c1e912b4b362eaff5c9ba99
Leejoongjae123 <ljj3347@naver.com>
2024-08-08 23:20:06 +0900

    240808

--- src/components/sections/pageTitle.jsx
+++ src/components/sections/pageTitle.jsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/utils';
 
 const PageTitle = ({ pageName, breadcrumbLink, breadcrumbCurrent, className }) => {
     return (
-        <div className={cn(`lg:pt-[188px] pt-28 lg:mb-15 mb-9 bg-no-repeat bg-cover bg-[center_bottom] overflow-hidden relative z-[1] after:absolute after:w-full after:h-full after:top-0 after:left-0 after:bg-[rgba(0,31,63,0.4)] after:backdrop-blur after:z-[-1] bg-[url('/images/banner/hero-about1-1.jpg')]`, className )}>
+        <div className={cn(`lg:pt-[188px] pt-28 lg:mb-15 mb-9 bg-no-repeat bg-cover bg-[center_bottom] overflow-hidden relative z-[1] after:absolute after:w-full after:h-full after:top-0 after:left-0 after:bg-[rgba(0,31,63,0.4)] after:backdrop-blur after:z-[-1] bg-[url('/images/banner/airplane2.png')]`, className )}>
             <div className='pb-[130px] pt-25 px-[15px]'>
                 <div className='flex flex-col items-center'>
                     <Title size={"5xl"} className={"text-white"}>{pageName}</Title>
