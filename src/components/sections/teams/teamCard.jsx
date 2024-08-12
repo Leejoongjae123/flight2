09c2ab26e5fb31ac39ef4d84abef1d571c817957
Leejoongjae123 <ljj3347@naver.com>
2024-08-12 18:24:07 +0900

    240812

--- src/components/sections/teams/teamCard.jsx
+++ src/components/sections/teams/teamCard.jsx
@@ -7,8 +7,8 @@ import { cn } from '@/lib/utils'
 const TeamCard = ({ id, name, position, src, bgColor, link }) => {
   return (
     <SlideUp id={id}>
-      <div className='relative h-25 w-150 group after:absolute after:w-full after:h-full after:top-0 after:left-0 after:transition-all after:duration-700 hover:after:bg-[linear-gradient(0deg,_rgba(0,0,0,0.20)_0%,_rgba(0,0,0,0.20)_100%)] '>
-        <Image src={src} alt='team-1' className='w-auto h-full object-cover' fill/>
+      <div className='relative p-5 h-25 w-150 group after:absolute after:w-full after:h-full after:top-0 after:left-0 after:transition-all after:duration-700 hover:after:bg-[linear-gradient(0deg,_rgba(0,0,0,0.20)_0%,_rgba(0,0,0,0.20)_100%)] '>
+        <Image src={src} alt='team-1' className='w-auto h-full object-contain' fill/>
 
       </div>
     </SlideUp>
