cea8c0a2730b06ffaba2c7f3ded9749ebcf4a7d2
Leejoongjae123 <ljj3347@naver.com>
2024-08-22 17:56:18 +0900

    240822

--- src/app/(home)/layout.js
+++ src/app/(home)/layout.js
@@ -33,9 +33,19 @@ const dm_sans = DM_Sans({
 export const metadata = {
   title: "UND 항공대학교 글로벌 패스웨이",
   description:
-    "NextPro is a modern Next.js and Tailwind CSS Template there features General Marketing, Social Media Marketing, AI in Marketing, Paid Advertising, Video Marketing, Analytics and Reporting, Industry news & Trends, E-commerce Marketing",
+    "국내 유일 UND 공식인증업체 미국 4년제 주립항공대학교 UND 입학, 항공유학, 조종사유학,  고등학생 유학, 미국조종사 취업, 미국 비행교관과정 유학상담",
   icons: {
     icon: "/images/logo/onlylogo.png",
+  },
+  openGraph: {
+    images: [
+      {
+        url: "/images/ogimage.png",
+        width: 800,
+        height: 600,
+        alt: "OG Image",
+      },
+    ],
   },
 };
 
--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -1,33 +1,42 @@
-import React from 'react'
-import Approach from '@/src/components/sections/approach'
-import BlogTwo from '@/src/components/sections/blogs/blogTwo'
-import CountDown from '@/src/components/sections/countDown'
-import Dashboard from '@/src/components/sections/dashboard'
-import HeroFive from '@/src/components/sections/heroes/heroFive'
-import StoryFive from '@/src/components/sections/ourStories/storyFive'
-import PlatformRating from '@/src/components/sections/platformRating'
-import PriceThree from '@/src/components/sections/pricing/priceThree'
-import ProvideTwo from '@/src/components/sections/provides/provideTwo'
-import ServiceSix from '@/src/components/sections/services/serviceSix'
-import ServiceThree from '@/src/components/sections/services/servicesThree'
-import SubscribeOne from '@/src/components/sections/subscribes/subscribeOne'
-import SubscribeTwo from '@/src/components/sections/subscribes/subscribeTwo'
-import TestimonialTwo from '@/src/components/sections/testimonials/testimonialTwo'
-import HeaderOne from '@/src/components/sections/headers/headerOne'
-import FooterOne from '@/src/components/sections/footers/footerOne'
-import FaqTwo from '@/src/components/sections/faqs/faqTwo'
-import FaqOne from '@/src/components/sections/faqs/faqOne'
-import Open from '@/src/components/sections/UnderConstruction'
+import React from "react";
+import Approach from "@/src/components/sections/approach";
+import BlogTwo from "@/src/components/sections/blogs/blogTwo";
+import CountDown from "@/src/components/sections/countDown";
+import Dashboard from "@/src/components/sections/dashboard";
+import HeroFive from "@/src/components/sections/heroes/heroFive";
+import StoryFive from "@/src/components/sections/ourStories/storyFive";
+import PlatformRating from "@/src/components/sections/platformRating";
+import PriceThree from "@/src/components/sections/pricing/priceThree";
+import ProvideTwo from "@/src/components/sections/provides/provideTwo";
+import ServiceSix from "@/src/components/sections/services/serviceSix";
+import ServiceThree from "@/src/components/sections/services/servicesThree";
+import SubscribeOne from "@/src/components/sections/subscribes/subscribeOne";
+import SubscribeTwo from "@/src/components/sections/subscribes/subscribeTwo";
+import TestimonialTwo from "@/src/components/sections/testimonials/testimonialTwo";
+import HeaderOne from "@/src/components/sections/headers/headerOne";
+import FooterOne from "@/src/components/sections/footers/footerOne";
+import FaqTwo from "@/src/components/sections/faqs/faqTwo";
+import FaqOne from "@/src/components/sections/faqs/faqOne";
+import Open from "@/src/components/sections/UnderConstruction";
+import Head from "next/head";
 
-const metadata = {
-  title: '글로벌 패스웨이',
-  description: '글로벌 패스웨이',
-}
+// const metadata = {
+//   title: '글로벌 패스웨이',
+//   description: '글로벌 패스웨이',
+// }
+
+// export const metadata = {
+//   title: "UND 항공대학교 글로벌 패스웨이",
+//   description:
+//     "항공 유학 서비스를 제공하는 웹사이트입니다.",
+//   icons: {
+//     icon: "/images/logo/onlylogo.png",
+//   },
+// };
 
 const Home = () => {
   return (
     <>
-      
       <main>
         {/* <Open></Open> */}
         <HeroFive />
